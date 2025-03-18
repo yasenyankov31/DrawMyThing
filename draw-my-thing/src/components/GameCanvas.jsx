65fee8726a82e924e49097ef0e4dcd8e79436df5
Yasen <yasenyankov21@gmail.com>
2025-03-19 00:23:41 +0200

    Updated all canvas tools code

--- draw-my-thing/src/components/GameCanvas.jsx
+++ draw-my-thing/src/components/GameCanvas.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import "./GameCanvas.css";
 
 const GameCanvas = () => {
   const canvasRef = useRef(null);
-  const [ctx, setCtx] = useState(null);
-  const [color, setColor] = useState("black");
+  const ctxRef = useRef(null);
+  const penSizeRef = useRef(20);
+  const selectedToolRef = useRef("pen");
+  const [color, setColor] = useState("#000000");
+  const [penSize, setPenSize] = useState(20);
+  const [selectedColor, setSelectedColor] = useState("#000000");
   const [selectedTool, setSelectedTool] = useState("pen");
   const [pos, setPos] = useState({ x: 0, y: 0 });
 
@@ -26,213 +30,277 @@ const GameCanvas = () => {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    setCtx(context);
+    ctxRef.current = canvas.getContext("2d", { willReadFrequently: true });
+    ctxRef.current.fillStyle = "#ffffff";
+    ctxRef.current.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
 
-    document.addEventListener("mousemove", draw);
-    document.addEventListener("mousedown", handleMouseDown);
-    document.addEventListener("mouseenter", handleMouseDown);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mousedown", setPosition);
+    canvas.addEventListener("click", fillArea);
 
     return () => {
-      document.removeEventListener("mousemove", draw);
-      document.removeEventListener("mousedown", handleMouseDown);
-      document.removeEventListener("mouseenter", handleMouseDown);
+      canvas.removeEventListener("mousemove", draw);
+      canvas.removeEventListener("mousedown", setPosition);
+      canvas.removeEventListener("click", fillArea);
     };
-  }, [pos, ctx]);
+  }, [pos]);
 
   const setPosition = (e) => {
     const canvas = canvasRef.current;
     const rect = canvas.getBoundingClientRect();
-    setPos({
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top,
-    });
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    setPos({ x, y });
+    if (
+      selectedToolRef.current === "pen" ||
+      selectedToolRef.current === "eraser"
+    ) {
+      ctxRef.current.beginPath();
+      ctxRef.current.arc(x, y, penSizeRef.current / 2, 0, Math.PI * 2);
+      ctxRef.current.fillStyle = selectedTool === "pen" ? color : "#FFFFFF";
+      ctxRef.current.fill();
+    }
   };
 
   const draw = (e) => {
-    if (e.buttons !== 1 || selectedTool !== "pen") return;
+    if (
+      e.buttons !== 1 ||
+      (selectedToolRef.current !== "pen" &&
+        selectedToolRef.current !== "eraser")
+    )
+      return;
 
     const canvas = canvasRef.current;
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = color;
+    ctxRef.current.beginPath();
+    ctxRef.current.lineWidth = penSizeRef.current;
+    ctxRef.current.lineCap = "round";
+    ctxRef.current.strokeStyle =
+      selectedToolRef.current === "pen" ? color : "#FFFFFF";
 
-    ctx.moveTo(pos.x, pos.y);
-    setPosition(e);
-    ctx.lineTo(x, y);
+    ctxRef.current.moveTo(pos.x, pos.y);
+    ctxRef.current.lineTo(x, y);
+    setPos({ x, y });
 
-    ctx.stroke();
-  };
-
-  const handleMouseDown = (e) => {
-    if (selectedTool === "bucket") {
-      const canvas = canvasRef.current;
-      const rect = canvas.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      // Get the color of the clicked pixel
-      const pixelColor = ctx.getImageData(x, y, 1, 1).data;
-      // Convert the selected color to RGBA format
-      const fillColor = hexToRgbA(color);
-      if (
-        pixelColor[0] !== fillColor[0] ||
-        pixelColor[1] !== fillColor[1] ||
-        pixelColor[2] !== fillColor[2]
-      ) {
-        floodFill(ctx, x, y, pixelColor, fillColor);
-      }
-    }
-    setPosition(e);
+    ctxRef.current.stroke();
   };
 
   const clearCanvas = () => {
     const canvas = canvasRef.current;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctxRef.current.clearRect(0, 0, canvas.width, canvas.height);
+    ctxRef.current.fillStyle = "#ffffff";
+    ctxRef.current.fillRect(0, 0, canvas.width, canvas.height);
   };
 
-  const floodFill = (ctx, x, y, targetColor, fillColor) => {
+  const componentToHex = (c) => {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  };
+
+  const rgbToHex = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  };
+
+  const getPixelColor = (ctx, x, y) => {
+    const pixel = ctx.getImageData(x, y, 1, 1).data;
+    return rgbToHex(pixel[0], pixel[1], pixel[2]);
+  };
+
+  const hexToRGB = (hex) => {
+    return [
+      parseInt(hex.substring(1, 3), 16),
+      parseInt(hex.substring(3, 5), 16),
+      parseInt(hex.substring(5, 7), 16),
+    ];
+  };
+
+  const colorMatch = (color1, color2, tolerance = 100) => {
+    const rgb1 = hexToRGB(color1);
+    const rgb2 = hexToRGB(color2);
+    return (
+      Math.abs(rgb1[0] - rgb2[0]) <= tolerance &&
+      Math.abs(rgb1[1] - rgb2[1]) <= tolerance &&
+      Math.abs(rgb1[2] - rgb2[2]) <= tolerance
+    );
+  };
+
+  const fillArea = (e) => {
+    if (selectedToolRef.current !== "bucket") return;
+
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.round(e.clientX - rect.left);
+    const y = Math.round(e.clientY - rect.top);
+    const oldColor = getPixelColor(ctxRef.current, x, y);
+
+    if (colorMatch(oldColor, color, 40)) return;
+
+    floodFill(ctxRef.current, x, y, oldColor, color);
+  };
+
+  const floodFill = (ctx, startX, startY, oldColor, newColor) => {
     const canvas = ctx.canvas;
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const pixelStack = [[x, y]];
-    const targetR = targetColor[0];
-    const targetG = targetColor[1];
-    const targetB = targetColor[2];
-    console.log(pixelStack);
+    const w = canvas.width;
+    const h = canvas.height;
+    const imageData = ctx.getImageData(0, 0, w, h);
+    const data = imageData.data;
 
-    while (pixelStack.length) {
-      const [currentX, currentY] = pixelStack.pop();
-      const pixelPos = (currentY * canvas.width + currentX) * 4;
+    const getColorAt = (x, y) => {
+      const index = (y * w + x) * 4;
+      return rgbToHex(data[index], data[index + 1], data[index + 2]);
+    };
 
-      if (
-        currentX < 0 ||
-        currentX >= canvas.width ||
-        currentY < 0 ||
-        currentY >= canvas.height
-      )
-        continue;
+    const setColorAt = (x, y, rgb) => {
+      const index = (y * w + x) * 4;
+      data[index] = rgb[0];
+      data[index + 1] = rgb[1];
+      data[index + 2] = rgb[2];
+      data[index + 3] = 255; // Fully opaque
+    };
 
-      const r = imageData.data[pixelPos];
-      const g = imageData.data[pixelPos + 1];
-      const b = imageData.data[pixelPos + 2];
+    if (getColorAt(startX, startY) !== oldColor) return;
 
-      if (r === targetR && g === targetG && b === targetB) {
-        imageData.data[pixelPos] = fillColor[0]; // Red
-        imageData.data[pixelPos + 1] = fillColor[1]; // Green
-        imageData.data[pixelPos + 2] = fillColor[2]; // Blue
-        imageData.data[pixelPos + 3] = fillColor[3]; // Alpha
+    const newRGB = hexToRGB(newColor);
 
-        pixelStack.push([currentX + 1, currentY]);
-        pixelStack.push([currentX - 1, currentY]);
-        pixelStack.push([currentX, currentY + 1]);
-        pixelStack.push([currentX, currentY - 1]);
-      }
+    const stack = [[startX, startY]];
+    while (stack.length) {
+      const [x, y] = stack.pop();
+
+      if (x < 0 || y < 0 || x >= w || y >= h) continue;
+      if (!colorMatch(getColorAt(x, y), oldColor)) continue;
+
+      setColorAt(x, y, newRGB);
+
+      stack.push([x + 1, y]);
+      stack.push([x - 1, y]);
+      stack.push([x, y + 1]);
+      stack.push([x, y - 1]);
     }
 
     ctx.putImageData(imageData, 0, 0);
   };
 
-  const hexToRgbA = (hex) => {
-    let r = 0,
-      g = 0,
-      b = 0,
-      a = 255;
-    if (hex.length === 7) {
-      r = parseInt(hex.slice(1, 3), 16);
-      g = parseInt(hex.slice(3, 5), 16);
-      b = parseInt(hex.slice(5, 7), 16);
-    } else if (hex.length === 4) {
-      r = parseInt(hex[1] + hex[1], 16);
-      g = parseInt(hex[2] + hex[2], 16);
-      b = parseInt(hex[3] + hex[3], 16);
-    }
-    return [r, g, b, a];
+  const handleSliderChange = (e) => {
+    const newSize = parseInt(e.target.value, 10);
+    setPenSize(newSize);
+    penSizeRef.current = newSize;
   };
 
   return (
     <div className="canvas-container">
-      {selectedTool}
       <canvas ref={canvasRef} width={800} height={600} className="canvas" />
-      <div className="controls-container">
-        <div className="color-palette">
-          {colors.map((color, index) => (
-            <div
-              key={index}
-              className="color-box"
-              style={{ backgroundColor: color }}
-              onClick={() => setColor(color)}
-            />
-          ))}
-        </div>
-        <div className="buttons-container">
-          <Button
-            className="button"
-            onClick={() => {
-              setSelectedTool("pen");
-            }}
-          >
-            Pen
-            <img
-              className="p-1"
-              width={"30px"}
-              height={"30px"}
-              src="../../pen.png"
-              alt="Pen"
-            />
-          </Button>
-          <Button
-            className="button"
-            onClick={() => {
-              setSelectedTool("bucket");
-            }}
-          >
-            Bucket
-            <img
-              className="p-1"
-              width={"30px"}
-              height={"30px"}
-              src="../../bucket.png"
-              alt="Bucket"
-            />
-          </Button>
-          <Button
-            className="button"
-            onClick={() => {
-              setColor("#FFFFFF");
-            }}
-          >
-            Eraser
-            <img
-              className="p-1"
-              width={"30px"}
-              height={"30px"}
-              src="../../eraser.png"
-              alt="Eraser"
-            />
-          </Button>
-          <Button
-            className="button"
-            onClick={() => {
-              clearCanvas();
-            }}
-          >
-            Clear All
-            <img
-              className="p-1"
-              width={"30px"}
-              height={"30px"}
-              src="../../bin.png"
-              alt="Clear All"
-            />
-          </Button>
-        </div>
-      </div>
+      <Row>
+        <Col md={8}>
+          {" "}
+          <div className="controls-container">
+            <div className="color-palette">
+              {colors.map((color, index) => (
+                <div
+                  key={index}
+                  className={`color-box ${
+                    selectedColor === color
+                      ? selectedColor === "#000000"
+                        ? "selected"
+                        : "selected-color"
+                      : ""
+                  }`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => {
+                    setColor(color);
+                    setSelectedColor(color);
+                  }}
+                />
+              ))}
+            </div>
+            <div className="buttons-container">
+              <div
+                className={`button ${selectedTool === "pen" ? "selected" : ""}`}
+                onClick={() => {
+                  selectedToolRef.current = "pen";
+                  setSelectedTool("pen");
+                }}
+              >
+                Pen
+                <img
+                  className="p-1"
+                  width="30px"
+                  height="30px"
+                  src="../../pen.png"
+                  alt="Pen"
+                />
+              </div>
+
+              <div
+                className={`button ${
+                  selectedTool === "bucket" ? "selected" : ""
+                }`}
+                onClick={() => {
+                  selectedToolRef.current = "bucket";
+                  setSelectedTool("bucket");
+                }}
+              >
+                Bucket
+                <img
+                  className="p-1"
+                  width="30px"
+                  height="30px"
+                  src="../../bucket.png"
+                  alt="Bucket"
+                />
+              </div>
+
+              <div
+                className={`button ${
+                  selectedTool === "eraser" ? "selected" : ""
+                }`}
+                onClick={() => {
+                  selectedToolRef.current = "eraser";
+                  setSelectedTool("eraser");
+                }}
+              >
+                Eraser
+                <img
+                  className="p-1"
+                  width="30px"
+                  height="30px"
+                  src="../../eraser.png"
+                  alt="Eraser"
+                />
+              </div>
+
+              <div className="button" onClick={clearCanvas}>
+                Clear All
+                <img
+                  className="p-1"
+                  width="30"
+                  height="30"
+                  src="../../bin.png"
+                  alt="Clear All"
+                />
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col md={4}>
+          {" "}
+          <Form.Label>Pen Size</Form.Label>
+          <Form.Range
+            value={penSize}
+            min={1}
+            max={40}
+            onChange={handleSliderChange}
+            className="custom-slider"
+          />
+        </Col>
+      </Row>
     </div>
   );
 };
